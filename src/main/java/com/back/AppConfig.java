69a2f0400e193cc2e50e30502ca88af99dfc7b66
Jung Yunseo <yunseo2488@naver.com>
2025-08-29 11:16:50 +0900

    프록시

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -12,16 +12,38 @@ import org.springframework.context.annotation.Lazy;
 @RequiredArgsConstructor
 public class AppConfig {
 
+    // 왜 lazy 와 self 를 사용해야하는가?
+    // final 로 하면 계속 순환 -> 본인을 만들기 위해 본인이 필요함
+    // 그래서 생성자로 받지 않고(final X) autowired 선언(얘는 생성할 때 안 받음)
+    // 근데 Autowired 만 선언(필드 입력 받는다)해준다고 해결되는건 아님
+    // new AppConfig.self = new AppConfig() 가 반복
+
+    // lazy 가 해결해줌
+    // 프록시는 리얼이 갖고 있는 기능을 다 갖고 있어야함 -> 진짜에 work1 이 있으면 프록시에도 work1이 있어야함
+    // @Lazy - 프록시로 만들어주는 역할
+    // self 가 진짜에 존재 -> lazy 로 선언 -> 프록시에도 self 를 만든다
+    // 모든 객체는 this 를 갖고 있음 = 자기 자신을 가리킴 -> self 를 빼면 기본으로 this 가 붙어있음
+    // 프록시에서 this -> 진짜의 self 를 가리킨다
+
+
+
+
+
+
     @Autowired
     @Lazy
-    private  AppConfig self; // final -> 생성자로만 주입 가능
-    // final 없애고 autowired 로 필드 주입
+    private AppConfig self;
 
     @Bean
     public ApplicationRunner myApplicationRunner3() {
         return args -> {
-               self.work1();
-               self.work2();
+                // 리얼 객체의 메서드 호출
+                this.work1();
+                this.work2();
+
+                // 프록시 객체의 메서드 호출
+                self.work1();
+                self.work2();
         };
     }
 
